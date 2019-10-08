18e85f750dff8a623a06b607180b4ad48199d832
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-10-08 09:17:33 +0200

    Chat ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact name="Keith Stone" avatar="https://randomuser.me/api/portraits/men/13.jpg" online={false}/>
+      <Contact name="Jeanette Sullivan" avatar="https://randomuser.me/api/portraits/women/57.jpg" online={false}/>
+      <Contact name="Nellie Beck" avatar="https://randomuser.me/api/portraits/women/88.jpg" online={true} />
     </div>
   );
 }
